bd437aab05ff7641344e0b39633ab55d05082601
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-13 18:11:45 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:math';
 import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:file_picker/file_picker.dart';
