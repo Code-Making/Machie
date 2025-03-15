7c09fda7b295ebaf4d1cf333ba146322749ef510
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-15 14:56:17 +0100

    update MainActivity.kt

--- android/app/src/main/kotlin/com/thomasdumonet/machine/MainActivity.kt
+++ android/app/src/main/kotlin/com/thomasdumonet/machine/MainActivity.kt
@@ -116,7 +116,7 @@ class MainActivity: FlutterActivity() {
     
 private fun writeIntentFile(uri: Uri, content: String): Boolean {
     return try {
-        contentResolver.openFileDescriptor(uri, "wt")?.use { pfd ->
+        contentResolver.openFileDescriptor(uri, "rw")?.use { pfd ->
             FileOutputStream(pfd.fileDescriptor).use { fos ->
                 fos.write(content.toByteArray())
                 true
