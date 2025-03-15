548e74c6b606594128859d050652678cd525f99e
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-15 13:50:30 +0100

    update MainActivity.kt

--- android/app/src/main/kotlin/com/thomasdumonet/machine/MainActivity.kt
+++ android/app/src/main/kotlin/com/thomasdumonet/machine/MainActivity.kt
@@ -10,6 +10,7 @@ import java.nio.charset.StandardCharsets
 
 import android.provider.DocumentsContract
 import androidx.annotation.NonNull
+import androidx.annotation.SuppressLint
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
