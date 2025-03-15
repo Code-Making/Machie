370827e345e3d76208b80dcc6a3770f654c204f0
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-15 22:53:21 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -396,9 +396,7 @@ Future<bool> _checkFileModified(String uri) async {
                       fontSize: 14,
                       fontFamily: 'FiraMono',
                       codeTheme: CodeHighlightTheme(
-                      languages: {
-                        _getLanguageMode(tab.uri)
-                      },         
+                      languages: _getLanguageMode(tab.uri),         
                       theme: atomOneDarkTheme,
                       ),
                     ),
