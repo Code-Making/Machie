ba09fddaa942cd71022db89d7bbeb512808e2815
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-15 14:23:14 +0100

    update MainActivity.kt

--- android/app/src/main/kotlin/com/thomasdumonet/machine/MainActivity.kt
+++ android/app/src/main/kotlin/com/thomasdumonet/machine/MainActivity.kt
@@ -118,7 +118,7 @@ private fun handleIntent(intent: Intent) {
     if (intent.action == Intent.ACTION_VIEW) {
         intent.data?.let { uri ->
             val writable = try {
-                contentResolver.openFileDescriptor(uri, "w")?.close()
+                contentResolver.openFileDescriptor(uri, "rw")?.close()
                 true
             } catch (e: SecurityException) {
                 false
