a125ef875915c5873cbf7e679502afbe700d93f1
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-21 00:19:38 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -788,7 +788,7 @@ class _FileOperationsFooter extends ConsumerWidget {
           onPressed: () async {
             final uri = await ref.read(fileHandlerProvider).openFile();
             if (uri != null) {
-              ref.read(tabManagerProvider.notifier).openFileTab(uri);
+              ref.read(tabManagerProvider.notifier).openFile(uri);
             }
           },
         ),
