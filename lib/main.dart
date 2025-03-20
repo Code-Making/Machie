e994b5bf15db53668f5b50fe2f131b82eb7122aa
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-20 18:29:03 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:core';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:async';
+import 'package:collection/collection.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:permission_handler/permission_handler.dart';
