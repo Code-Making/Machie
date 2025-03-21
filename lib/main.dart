b800c1d0762ce755424afb3364c77ad79865c0c1
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-21 19:56:52 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -302,6 +302,7 @@ class CodeEditorPlugin implements EditorPlugin {
     return CodeEditor(
       controller: codeTab.controller,
       style: _editorStyleFor(codeTab.uri),
+      wordWrap: false,
     );
   }
 
