74440df39d9690aebd2c0b020b0e0c0b74c34507
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-22 21:13:00 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:collection/collection.dart';
 
 import 'package:saf_stream/saf_stream.dart';
 import 'package:saf_util/saf_util.dart';
+import 'package:saf_util/saf_util_platform_interface.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:flutter/material.dart';
