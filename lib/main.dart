e65099a64b46a5572dcc4530216685029f34485b
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-24 20:27:51 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -108,7 +108,7 @@ final sessionManagerProvider = Provider<SessionManager>((ref) {
   return SessionManager(
     ref.watch(fileHandlerProvider),
     ref.watch(pluginRegistryProvider),
-    ref.watch(sharedPreferencesProvider)
+    ref.watch(sharedPreferencesProvider).requireValue,
   );
 });
 
