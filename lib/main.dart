fe4724c4019246c6f9ac2cf92fd645efc9ff3518
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-30 12:25:37 +0200

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3539,7 +3539,7 @@ class RecipeTexPlugin implements EditorPlugin {
   }
 
   InstructionStep _parseInstruction(String instruction) {
-    final titleMatch = RegExp(r'\\textbf\{\\large\s*(.*?)\}\s*(.*)',unicode: true).firstMatch(instruction);
+    final titleMatch = RegExp(r'\\textbf\{\\large\s*(.*?)\}\s*(.*)').firstMatch(instruction);
     print("instruction");
     print(titleMatch?.toString());
     return titleMatch != null 
