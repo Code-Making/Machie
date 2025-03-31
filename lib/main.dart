78d3e370f0bb6e103ceeb299a9b477577fe794d6
hommes-doutant <thomas.dumonet@gmail.com>
2025-03-31 22:46:39 +0200

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3908,8 +3908,7 @@ Ingredient({
     required this.unit,
     required this.name,
   }) : id = id ?? const Uuid().v4(); // Generate UUID if not provided
-  Ingredient(this.quantity, this.unit, this.name);
-  
+
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
