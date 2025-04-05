bb5fffb29b43c2eac9410f1263aadaa0fc924854
hommes-doutant <thomas.dumonet@gmail.com>
2025-04-05 18:11:52 +0200

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1227,12 +1227,20 @@ class CodeEditorPlugin implements EditorPlugin {
   };
   
   void _handleFocus(bool focus, CodeLineEditingController ctrl){
-      print("focus "+focus.toString());
-      if (focus == true){
-        WidgetsBinding.instance.addPostFrameCallback((_) {
+    if (focus) {
+      // Keyboard is starting to appear
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (!mounted) return;
+        // Wait for keyboard animation (e.g., 300ms)
+        Future.delayed(const Duration(milliseconds: 300), () {
+          if (!mounted) return;
+          final viewInsets = MediaQuery.viewInsetsOf(context);
+          if (viewInsets.bottom > 0) {
                ctrl.makeCursorVisible();
-            });
-        }
+          }
+        });
+      });
+    }
   }
 
   @override
