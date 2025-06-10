bd57b5a9ce9b2d94e4d9327678bfa9c0b72c778f
hommesdoutant <thomas.dumonet@gmail.com>
2025-06-10 17:05:33 +0200

    session imports

--- lib/session/session_models.dart
+++ lib/session/session_models.dart
@@ -3,8 +3,8 @@ import 'package:collection/collection.dart';
 import 'package:re_editor/re_editor.dart';
 import 'package:flutter/material.dart';
 
-import '../plugins/plugin_architecture.dart';
-import '../project/file_handler/file_handler.dart';
+import '../plugins/plugin_models.dart';
+import '../data/file_handler/file_handler.dart';
 
 @immutable
 class SessionState {
