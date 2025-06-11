1a258d01606d09c1529b4da91f206867b62e4e7e
hommesdoutant <thomas.dumonet@gmail.com>
2025-06-11 22:06:41 +0200

    new project type and small rrefactor

--- lib/project/project_models.dart
+++ lib/project/project_models.dart
@@ -4,36 +4,33 @@ import '../plugins/plugin_models.dart';
 import '../session/session_models.dart';
 import '../data/file_handler/file_handler.dart';
 
-// --- Enums ---
-enum ProjectType { local } // Ready for future types like 'remoteSsh'
+// DELETED: The enum is no longer needed. We'll use string IDs.
+// enum ProjectType { local }
 
 enum FileExplorerViewMode { sortByNameAsc, sortByNameDesc, sortByDateModified }
 
 // --- Models ---
 
-// Lightweight reference to a project, stored in global app state.
 class ProjectMetadata {
-  // ... (no changes here) ...
   final String id;
   final String name;
   final String rootUri;
-  final ProjectType projectType;
+  final String projectTypeId; // MODIFIED: from ProjectType to String
   final DateTime lastOpenedDateTime;
 
   ProjectMetadata({
     required this.id,
     required this.name,
     required this.rootUri,
-    this.projectType = ProjectType.local,
+    required this.projectTypeId, // MODIFIED
     required this.lastOpenedDateTime,
   });
 
-  // Serialization for SharedPreferences
   Map<String, dynamic> toJson() => {
     'id': id,
     'name': name,
     'rootUri': rootUri,
-    'projectType': projectType.name,
+    'projectTypeId': projectTypeId, // MODIFIED
     'lastOpenedDateTime': lastOpenedDateTime.toIso8601String(),
   };
 
@@ -42,15 +39,11 @@ class ProjectMetadata {
         id: json['id'],
         name: json['name'],
         rootUri: json['rootUri'],
-        projectType: ProjectType.values.firstWhere(
-          (e) => e.name == json['projectType'],
-          orElse: () => ProjectType.local,
-        ),
+        projectTypeId: json['projectTypeId'], // MODIFIED
         lastOpenedDateTime: DateTime.parse(json['lastOpenedDateTime']),
       );
 }
 
-// Abstract base class for all project types.
 abstract class Project {
   ProjectMetadata metadata;
   FileHandler fileHandler;
@@ -65,14 +58,14 @@ abstract class Project {
   String get id => metadata.id;
   String get name => metadata.name;
   String get rootUri => metadata.rootUri;
+  String get projectTypeId => metadata.projectTypeId; // NEW: Expose the type ID.
 
-  // NEW: Lifecycle methods defining the contract for all project types.
+  // NEW: Contract for serializing internal state (session, etc.)
+  Map<String, dynamic> toJson();
+
   Future<void> save();
-  // MODIFIED: Signature now requires a Ref to pass down to plugins.
   Future<void> close({required Ref ref});
 
-  // NEW: Session manipulation methods, returning a new immutable Project instance.
-  // The responsibility for this logic is moved from SessionService to here.
   Future<Project> openFile(DocumentFile file, {EditorPlugin? plugin, required Ref ref});
   Project switchTab(int index, {required Ref ref});
   Project reorderTabs(int oldIndex, int newIndex);
