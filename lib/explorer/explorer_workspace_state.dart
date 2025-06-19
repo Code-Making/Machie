4af5c0cddc757fea7ced203c0e6876b891e3c65a
hommesdoutant <thomas.dumonet@gmail.com>
2025-06-19 10:49:56 +0200

    Breaking refactor

--- lib/explorer/explorer_workspace_state.dart
+++ lib/explorer/explorer_workspace_state.dart
@@ -2,20 +2,20 @@
 import 'package:flutter/foundation.dart';
 
 @immutable
-class WorkspaceState {
+class ExplorerWorkspaceState {
   /// The ID of the last active explorer plugin for this project.
   final String activeExplorerPluginId;
 
   /// A generic map to hold the persisted state for each plugin, keyed by plugin ID.
   final Map<String, dynamic> pluginStates;
 
-  const WorkspaceState({
+  const ExplorerWorkspaceState({
     required this.activeExplorerPluginId,
     this.pluginStates = const {},
   });
 
-  factory WorkspaceState.fromJson(Map<String, dynamic> json) {
-    return WorkspaceState(
+  factory ExplorerWorkspaceState.fromJson(Map<String, dynamic> json) {
+    return ExplorerWorkspaceState(
       activeExplorerPluginId:
           json['activeExplorerPluginId'] ?? 'com.machine.file_explorer',
       pluginStates: Map<String, dynamic>.from(json['pluginStates'] ?? {}),
