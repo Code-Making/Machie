8e95b7634537f74b541c5c2cb7d34911f738ab2c
hommesdoutant <thomas.dumonet@gmail.com>
2025-06-20 10:04:26 +0200

    typo

--- lib/editors/plugins/plugin_registry.dart
+++ lib/editors/plugins/plugin_registry.dart
@@ -4,7 +4,7 @@ import 'plugin_models.dart';
 import 'code_editor/code_editor_plugin.dart';
 import 'glitch_editor/glitch_editor_plugin.dart'; // NEW IMPORT
 
-import 'plugin_models.dart';
+export 'plugin_models.dart';
 
 final pluginRegistryProvider = Provider<Set<EditorPlugin>>(
   (_) => {
