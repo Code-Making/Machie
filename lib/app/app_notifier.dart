763db43d3ff139b289991e865bab2e3d5e27aadf
hommesdoutant <thomas.dumonet@gmail.com>
2025-06-22 20:32:36 +0200

    oubli

--- lib/app/app_notifier.dart
+++ lib/app/app_notifier.dart
@@ -280,6 +280,26 @@ class AppNotifier extends AsyncNotifier<AppState> {
     final newProject = _editorService.closeTab(project, index);
     _updateStateSync((s) => s.copyWith(currentProject: newProject));
   }
+  
+    // NEW: Handler for file events.
+  void _handleFileOperationEvent(FileOperationEvent event) {
+    final project = state.value?.currentProject;
+    if (project == null) return;
+    
+    switch (event) {
+      case FileRenameEvent(oldFile: final oldFile, newFile: final newFile):
+        final newProject = _editorService.updateTabFile(project, oldFile.uri, newFile);
+        _updateStateSync((s) => s.copyWith(currentProject: newProject));
+        break;
+      case FileDeleteEvent(deletedFile: final deletedFile):
+        final tabIndex = project.session.tabs.indexWhere((t) => t.file.uri == deletedFile.uri);
+        if (tabIndex != -1) {
+          final newProject = _editorService.closeTab(project, tabIndex);
+          _updateStateSync((s) => s.copyWith(currentProject: newProject));
+        }
+        break;
+    }
+  }
 
   void updateCurrentTab(EditorTab newTab) {
     final project = state.value?.currentProject;
