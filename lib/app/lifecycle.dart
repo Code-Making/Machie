275137391c4e051b3ecf19903ebef748bd3b7b03
hommesdoutant <thomas.dumonet@gmail.com>
2025-06-26 15:15:07 +0200

    added logs

--- lib/app/lifecycle.dart
+++ lib/app/lifecycle.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import '../logs/logs_provider.dart';
 import 'app_notifier.dart';
 
 // Handles app lifecycle events, primarily for saving state.
 class LifecycleHandler extends ConsumerStatefulWidget {
   final Widget child;
-  const LifecycleHandler({super.key, required this.child});
+  final Talker talker;
+  const LifecycleHandler({super.key, required this.child, required this.talker});
   @override
   ConsumerState<LifecycleHandler> createState() => _LifecycleHandlerState();
 }
@@ -27,6 +28,7 @@ class _LifecycleHandlerState extends ConsumerState<LifecycleHandler>
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) async {
+    talker.info("Changing app state to $state");
     // When the app is paused or detached, trigger a save of the entire app state.
     if (state == AppLifecycleState.paused ||
         state == AppLifecycleState.detached) {
