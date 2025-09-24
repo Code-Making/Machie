28199b882e99d104fb45a1d4188d18f185b83ef8
hommes-doutant <thomas.dumonet@gmail.com>
2025-09-24 20:21:59 +0200

    fix

--- lib/editor/plugins/markdown_editor/markdown_theme.dart
+++ lib/editor/plugins/markdown_editor/markdown_theme.dart
@@ -3,8 +3,8 @@
 // =========================================
 
 import 'package:appflowy_editor/appflowy_editor.dart';
-import 'package.flutter/gestures.dart';
-import 'package.flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MarkdownEditorTheme {
