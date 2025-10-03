fc109d861f4ff46839dd990e216a8c10d3c1d157
hommes-doutant <thomas.dumonet@gmail.com>
2025-10-03 14:20:11 +0200

    fix

--- lib/app/lifecycle.dart
+++ lib/app/lifecycle.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../logs/logs_provider.dart';
 import 'app_notifier.dart';
+import '../project/services/hot_state_cache_service.dart';
 
 // Handles app lifecycle events, primarily for saving state.
 class LifecycleHandler extends ConsumerStatefulWidget {
