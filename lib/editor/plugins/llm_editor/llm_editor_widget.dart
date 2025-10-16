25209a1714585a24f43cf9535b074551a1795321
hommes-doutant <thomas.dumonet@gmail.com>
2025-10-16 22:23:03 +0200

    fix

--- lib/editor/plugins/llm_editor/llm_editor_widget.dart
+++ lib/editor/plugins/llm_editor/llm_editor_widget.dart
@@ -650,7 +650,8 @@ class _CodeBlockWrapperState extends ConsumerState<_CodeBlockWrapper> {
       ),
     );
 
-    final editorHeight = _controller.codeLines.length * _style.fontSize * _style.fontHeight + 16.0;
+    final fontHeight = _style.fontHeight ?? 1;
+    final editorHeight = _controller.codeLines.length * _style.fontSize * fontHeight + 16.0;
 
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 8.0),
