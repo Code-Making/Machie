c7fa4beea86976abb418528844a5106eeda23548
hommes-doutant <thomas.dumonet@gmail.com>
2026-01-17 20:16:44 +0100

    fix

--- lib/editor/plugins/texture_packer/texture_packer_editor_widget.dart
+++ lib/editor/plugins/texture_packer/texture_packer_editor_widget.dart
@@ -424,7 +424,10 @@ class TexturePackerEditorWidgetState extends EditorWidgetState<TexturePackerEdit
     for (final file in result.files) {
       final imageProjectRelativePath = repo.fileHandler.getPathForDisplay(file.uri, relativeTo: project.rootUri);
       
-      final pathRelativeToTpacker = p.relative(imageProjectRelativePath, from: tpackerDirectory).replaceAll(r'\', '/');
+      final pathRelativeToTpacker = repo.calculateRelativePath(
+        fromContext: tpackerProjectRelativePath, 
+        toTarget: imageProjectRelativePath
+      );
 
       SlicingConfig config = const SlicingConfig();
       if (result.asSprites) {
