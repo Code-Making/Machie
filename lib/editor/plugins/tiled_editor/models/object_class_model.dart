ba7ec10428c6ae52acdde86759a2918bcc3cb38b
hommes-doutant <thomas.dumonet@gmail.com>
2026-01-21 10:12:48 +0100

    enum

--- lib/editor/plugins/tiled_editor/models/object_class_model.dart
+++ lib/editor/plugins/tiled_editor/models/object_class_model.dart
@@ -1,28 +1,43 @@
 import 'dart:ui';
 import 'package:flutter/foundation.dart';
 
-enum ClassMemberType { string, int, float, bool, color, file }
+enum ClassMemberType { 
+  string, 
+  int, 
+  float, 
+  bool, 
+  color, 
+  file,
+  enum_, // 'enum' is a reserved keyword
+}
 
+@immutable
 class ClassMemberDefinition {
   final String name;
   final ClassMemberType type;
   final dynamic defaultValue;
+  final List<String>? options; // New field for enum options
 
   const ClassMemberDefinition({
     required this.name,
     required this.type,
     this.defaultValue,
+    this.options,
   });
 
   factory ClassMemberDefinition.fromJson(Map<String, dynamic> json) {
     return ClassMemberDefinition(
-      name: json['name'],
-      type: _parseType(json['type']),
+      name: json['name'] as String? ?? 'unnamed_member',
+      type: _parseType(json['type'] as String?),
       defaultValue: json['value'],
+      options: (json['options'] as List?)?.map((e) => e.toString()).toList(),
     );
   }
 
-  static ClassMemberType _parseType(String type) {
+  static ClassMemberType _parseType(String? type) {
+    if (type == null) return ClassMemberType.string;
+    if (type.toLowerCase() == 'enum') return ClassMemberType.enum_;
+    
     return ClassMemberType.values.firstWhere(
       (e) => e.name == type.toLowerCase(),
       orElse: () => ClassMemberType.string,
