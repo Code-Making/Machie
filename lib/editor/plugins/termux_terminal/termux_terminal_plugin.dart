e14064bdbd77909cbd7d88a9ab5161675f693151
hommes-doutant <thomas.dumonet@gmail.com>
2026-01-25 11:03:35 +0100

    fix

--- lib/editor/plugins/termux_terminal/termux_terminal_plugin.dart
+++ lib/editor/plugins/termux_terminal/termux_terminal_plugin.dart
@@ -16,6 +16,7 @@ import 'termux_hot_state.dart';
 import 'termux_hot_state_adapter.dart';
 import 'widgets/termux_terminal_widget.dart';
 import 'widgets/termux_settings_widget.dart';
+import '../../../command/command_widgets.dart';
 
 class TermuxTerminalPlugin extends EditorPlugin {
   static const String pluginId = 'com.machine.termux_terminal';
